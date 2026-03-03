41d29be2cab370b47627c6c3788de15bbf4ddcdf
Lionel <lionelkouame@gmail.com>
2026-03-03 06:34:45 +0100

    feat(song-management): #54 add Contributor port

---
+++ src/SongManagement/Domain/Port/ContributorRepositoryInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\SongManagement\Domain\Port;
+
+use App\SongManagement\Domain\Model\Contributor\Contributor;
+use App\SongManagement\Domain\Model\Contributor\ContributorId;
+
+/**
+ * Driven port for Contributor persistence.
+ *
+ * Defined in the Domain layer, implemented in the Infrastructure layer.
+ *
+ * @author Lionel KOUAME
+ */
+interface ContributorRepositoryInterface
+{
+    public function save(Contributor $contributor): void;
+
+    public function findById(ContributorId $id): ?Contributor;
+
+    /** @return Contributor[] */
+    public function findAll(): array;
+
+    public function delete(ContributorId $id): void;
+}
