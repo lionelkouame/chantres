eb61c09fc378e38d8abdbe11b8fb5ef3a644dbc8
Lionel <lionelkouame@gmail.com>
2026-03-05 11:50:24 +0100

    refactor(song-management): #54  move string trait

--- src/SongManagement/Domain/Model/Song/Title.php
+++ src/SongManagement/Domain/Model/Song/Title.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\SongManagement\Domain\Model\Song;
 
-use App\Shared\StringValueObjectInterface;
+use App\Shared\Domain\StringValueObjectInterface;
 
 /**
  * Title Value Object.
